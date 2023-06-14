13fd908fe61d200a727801e089f2a21d7cfca734
lnvitesace <lnvitesace@gmail.com>
2023-06-14 22:43:11 +0800

    fix comment

--- Chapter-2/Exercise-2-09.c
+++ Chapter-2/Exercise-2-09.c
@@ -1,5 +1,7 @@
 // Q: In a two's complement number system, x &= (x-1) deletes the rightmost 1-bit in x. Explain why.
-// A: In a two's complement number system, x - 1 will flip all the bits from the rightmost 1-bit to the least significant bit, and flip the rightmost 1-bit to 0. For example, 0110 - 1 = 0101, 0111 - 1 = 0110, 1000 - 1 = 0111. Then x &= (x - 1) will flip all the bits from the rightmost 1-bit to the least significant bit, and flip the rightmost 1-bit to 0, and flip the rightmost 0-bit to 1. For example, 0110 & 0101 = 0100, 0111 & 0110 = 0110, 1000 & 0111 = 0000. 
+// A: In a two's complement number system, x - 1 will flip all the bits from the rightmost 1-bit to the least significant bit, and flip the rightmost 1-bit to 0. 
+// For example, 0110 - 1 = 0101, 0111 - 1 = 0110, 1000 - 1 = 0111. Then x &= (x - 1) will flip all the bits from the rightmost 1-bit to the least significant bit, and flip the rightmost 1-bit to 0, and flip the rightmost 0-bit to 1. 
+// For instance, 0110 & 0101 = 0100, 0111 & 0110 = 0110, 1000 & 0111 = 0000. 
 
 // Use this observation to write a faster version of bitcount.
 int bitcount(unsigned x)
